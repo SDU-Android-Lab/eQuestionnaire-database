538848206c007cf5acb7eb5d031e9f0d8cbe5a8d
sduxzz <825502451@qq.com>
2012-07-20 15:26:38 +0800

    first commit

---
+++ Util/src/test/Test.java
@@ -0,0 +1,22 @@
+/*
+ * Util test Test.java
+ * 2012 2012-7-20 下午3:23:46
+ * @author zhenzxie
+ */
+package test;
+
+
+/**
+ * @author zhenzxie
+ *
+ */
+public class Test {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+	}
+	
+}
---
+++ Util/src/test/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Util test package-info.java
+ * 2012 2012-7-20 下午3:23:32
+ * @author zhenzxie
+ */
+/**
+ * @author zhenzxie
+ *
+ */
+package test;
